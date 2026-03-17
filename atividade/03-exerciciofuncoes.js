055425dd108e254219760fc581a04b566dd4d419
bruno777202 <b.medeiro@edu.senai.br>
2026-03-17 17:04:11 -0300

    idade

---
+++ atividade/03-exerciciofuncoes.js
@@ -0,0 +1,23 @@
+/*****************************************************
+Lista de Exercícios
+*****************************************************/
+/*
+1. Crie uma função que receba como parâmetro o ano de nascimento de uma pessoa a retorne sua idade.
+
+
+2. Crie uma função que receba a quantidade de combustível de um carro e retorne quantos Kms ele pode viajar com esse combustível. Considere que o
+ veículo faz uma média de 12 Km/L.
+*/
+
+function subtrair (valornascimento, valoridade) {let valornascimento = - (valoridade) 
+    return valornascimento 
+}
+
+let totalnascimento = 2010
+let totalidade = subtrair (totalnascimentoa,2026)
+let idade = totalnascimento - totalidade
+
+console.log(" '2010 - 2026' =", subtrair (2010,2026))
+console.log("subtrair v1 - v2", subtrair, v1, v2)
+
+
