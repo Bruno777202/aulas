1f17db5cbb23724de03c792e3901e3c3cf1e3c59
bruno777202 <b.medeiro@edu.senai.br>
2026-03-17 10:36:13 -0300

    revisão de javascript

---
+++ 07-revisão.js
@@ -0,0 +1,44 @@
+// comentario
+/*comentario de varias linhas de codigo 
+
+*/
+// imprimir informações na tela
+console.log("impremir informações na tela");
+//devlarasão  de variavel
+//sintaxe: let= variavel = a valor
+/*
+exenplo let idade= 45
+*/
+/*
+tipos de dados primários 
+números interos (int)= 10 
+número decimais (double) = 8.53
+carecteris alfanumeros (string) = "cristiano"
+números booleanos (bool) = false
+*/
+let idade = 45;
+let salario = 25864.57;
+let ehProfessor = false;
+let nome = "joão da Silva";
+
+console.log("Nome:", nome, "Idade", idade, "salário:", salario);
+
+/*
+ operação matemática 
+ + -> adição
+ - -> subtração
+ * -> mutplicação
+ / -> divisão
+ % -> restro da divisão
+*/
+let num1 = 10;
+let num2 = 2;
+
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let mutplicacao = num1 * num2;
+let divisao = num1 / num2;
+let restrodivisao = num1 % num2;
+
+console.log("somando", num1, "+ ", num2, " = ", soma);
+console.log("O restro da divisão do", num1, "por", num2, " = ", restrodivisao);
