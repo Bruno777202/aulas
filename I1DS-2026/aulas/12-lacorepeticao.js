b71b338470a445bc7c8bc7c7d7b54c74c17f2a4b
Bruno777202 <brunomedeiros@edu.senai.br>
2026-03-24 11:09:14 -0300

    losos = for

--- I1DS-2026/aulas/12-lacorepeticao.js
+++ I1DS-2026/aulas/12-lacorepeticao.js
@@ -1,6 +1,42 @@
 // laso de reptisão - 
 // O professor  te 
 
-for (let i = 0; i < 1000; i++) {
-       
+// i++ de quando ele almente a variavel sem travar
+
+for (let i = 0; i < 1000; i++) { //primeiro utiliza uma variavel 
+console.log("eu vou prestar atenção nas aulas e anotar tudo")
 }
+
+console.log("---------------------------------------------------")
+// escreva todos os números 1/20
+
+for (let i = 1; i <= 20; i++) {
+   console.log(i)
+    
+}
+console.log("---------------------------------------------------")
+// escreva todos números pares de 1 a 20
+for (let i= 0; i <=  20; i+= 2) {
+    console.log(i)
+    
+}
+console.log("-----------------------------------------------------")
+//
+
+for (let i = 1; i <= 20; i++) {
+    if(i % 2 == 0) console.log(i)
+    
+}
+// i = 2 ==0 
+
+
+//dado de 
+let numeros = [5, 6, 8, 14, 0, 9, 7, 2]
+let soma = 0
+
+console.log(numeros.length)
+
+for (let i = 0; i < numeros.length; i++) {
+    soma += numeros[i]
+}
+console.log(soma)
