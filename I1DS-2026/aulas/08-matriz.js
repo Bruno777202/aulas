49cb4282e1994e0a23d2bc9482f36e72552fdb1e
Bruno777202 <brunomedeiros@edu.senai.br>
2026-03-24 14:55:25 -0300

    matriz

---
+++ I1DS-2026/aulas/08-matriz.js
@@ -0,0 +1,32 @@
+// criando uma matriz
+let matriz = [
+    [6, 0, 1]
+    [7, 1, 2]
+    [6, 2, 8]
+    [1, 3, 9]
+];
+console.table(matriz);
+
+// obtendo elementos com base e seus indise
+console.log(matriz[1][2]);
+console.log(matriz[2][0]);
+//criado uma matriz 3x3
+//imprima os valores de diagnoticos principais
+//alterar os valers do iten matriz [1][2]
+
+let matrizes = [
+    [4, 7, 0]
+    [5, 8, 2]
+    [1, 3, 9]
+];
+
+console.table(matrizes);
+
+console.log(matrizes[0][0]);
+console.log(matrizes[1][1]);
+console.log(matrizes[2][2]);
+console.log(matrizes[0][2]);
+console.log(matrizes[2][0]);
+
+matrizes[1][2] = 20
+matrizes[2][0]
---
+++ atividade/08-lacorepeticao.js
@@ -0,0 +1,34 @@
+// dado o vetor calcule é exibe a quantidade de pares 
+//impares
+
+let numeros = [5, 6, 8, 14, 0, 6, 9, 7, 2]
+let par = 0;
+let inpar = 0;
+
+
+
+for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] % 2 == 0) par++ 
+  else inpar++
+}
+
+console.log("quantidade de pares", par)
+console.log("quantidade de inpares", inpar)
+
+// dado o vetor mutplique por 3
+console.table(numeros)
+
+for (let i = 0; i < numeros.length; i++) {
+    numeros[i] *= 3;
+}
+
+console.table(numeros)
+
+// dado uma matriz 2x3, imprima todos os sens elementos com as respectivas posições
+let matriz = [
+    [6, 4, 1]
+    [3, 7, 8]
+] 
+console.table(matriz)
+
+console.log(matriz)
