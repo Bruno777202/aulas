f2437f6fa4d4610c0faaed097472f1ce91b99a9c
Bruno777202 <b.medeiros@edu.senai.br>
2026-04-28 16:55:03 -0300

    index.scripts.css

--- Atividades/js/scripts.js
+++ Atividades/js/scripts.js
@@ -1,5 +1,7 @@
+let usercount = 0
+
 const cadastro = (event) =>{
-    event,preeventdefault();
+    event,preEventfauld();
          /*capturando os valores do elemento por id*/
     let nome = document.getElementById("nome").value
       let email = document.getElementById("e-mail").value
@@ -8,10 +10,31 @@ const cadastro = (event) =>{
       let lista = document.getElementById("lista");
 }
 //ciar um novo elemento li = nome
+usercount++;
+let id = usercount
+
+
 let novoitem = document.createElement("li");
-novoitem.innertext=nome + "-" + email
+novoitem = 
 
-lista.appendchild(novoitem);
+lista.appendChild;(novoitem);
 
-document.getElementById("nome").value = ""
-document.getElementById("email").value = ""
+document.getElementById("nome").value = nome 
+document.getElementById("email").value = email
+
+const editar = (id) =>{
+
+}
+
+const excluir = (id) =>{
+
+}
+const lista = document.getElementById("lista");
+const item = document.querySelectorAll("li");
+
+item.forEach(item =>)
+if (item.innerHTML.includes(id)){
+      item.remove()
+}
+
+
